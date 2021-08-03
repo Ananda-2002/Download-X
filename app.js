7512e856f828e39a8b24c3311ee15db14758d4b0
Ananda <anandagopaldutta2001@gmail.com>
2021-08-03 21:54:41 +0530

    All resulation available

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cp = require('child_process');
 const readline = require('readline');
 const ffmpeg = require('ffmpeg-static');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000; // process.env.PORT ||
 
 app.get("/", (req, res) => {
         // res.json({
@@ -24,72 +24,109 @@ app.get("/video-info", async function (req, res) {
 
 app.get("/download-video", async function (req, res) {
         const videoURL = req.query.videoURL;
+
+        res.header("Content-Disposition", `attachment;filename=video.mp4`)
+        const ref = videoURL
         const itag = req.query.itag;
-        let info = req.query.title;
-        // const ref = videoURL;
-        res.header("Content-Disposition", `attachment;filename=${info}.mp4`)
+        const title = req.query.title
+        let info = title;
+        const tracker = {
+                start: Date.now(),
+                audio: { downloaded: 0, total: Infinity },
+                video: { downloaded: 0, total: Infinity },
+                merged: { frame: 0, speed: '0x', fps: 0 },
+        };
 
-        // // Get audio and video streams
-        // const audio = ytdl(ref, { filter: 'audioonly' })
+        // Get audio and video streams
+        const audio = ytdl(ref, { filter: 'audioonly' })
+                .on('progress', (_, downloaded, total) => {
+                        tracker.audio = { downloaded, total };
+                });
+        const video = ytdl(ref, { filter: format => format.itag == itag })
+                .on('progress', (_, downloaded, total) => {
+                        tracker.video = { downloaded, total };
+                });
 
-        // const video = ytdl(ref, { filter: format => format.itag == itag })
+        // Prepare the progress bar
+        let progressbarHandle = null;
+        const progressbarInterval = 1000;
+        const showProgress = () => {
+                readline.cursorTo(process.stdout, 0);
+                const toMB = i => (i / 1024 / 1024).toFixed(2);
 
-        // // Start the ffmpeg child process
-        // const ffmpegProcess = cp.spawn(ffmpeg, [
-        //         // Remove ffmpeg's console spamming
-        //         '-loglevel', '8', '-hide_banner',
-        //         // Redirect/Enable progress messages
-        //         '-progress', 'pipe:3',
-        //         // Set inputs
-        //         '-i', 'pipe:4',
-        //         '-i', 'pipe:5',
-        //         // Map audio & video from streams
-        //         '-map', '0:a',
-        //         '-map', '1:v',
-        //         // Keep encoding
-        //         '-c:v', 'copy',
-        //         // Define output file
-        //         `${info}.mp4`,
-        //         '-f', 'matroska', 'pipe:6',
-        // ], {
-        //         windowsHide: true,
-        //         stdio: [
-        //                 /* Standard: stdin, stdout, stderr */
-        //                 'inherit', 'inherit', 'inherit',
-        //                 /* Custom: pipe:3, pipe:4, pipe:5 */
-        //                 'pipe', 'pipe', 'pipe', 'pipe'
-        //         ],
-        // });
+                // process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
+                // process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
 
-        // ffmpegProcess.on('close', () => {
-        //         console.log('done');
-        //         // Cleanup
-        //         process.stdout.write('\n\n\n\n');
-        // });
+                // process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
+                // process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
 
-        // // Link streams
-        // // FFmpeg creates the transformer streams and we just have to insert / read data
-        // ffmpegProcess.stdio[3].on('data', chunk => {
-        //         const lines = chunk.toString().trim().split('\n');
-        //         const args = {};
-        //         for (const l of lines) {
-        //                 const [key, value] = l.split('=');
-        //                 args[key.trim()] = value.trim();
+                // process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
+                // process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
 
+                // process.stdout.write(`running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
+                // readline.moveCursor(process.stdout, 0, -3);
+        };
 
-        //         }
+        // Start the ffmpeg child process
+        const ffmpegProcess = cp.spawn(ffmpeg, [
+                // Remove ffmpeg's console spamming
+                '-loglevel', '8', '-hide_banner',
+                // Redirect/Enable progress messages
+                '-progress', 'pipe:3',
+                // Set inputs
+                '-i', 'pipe:3',
+                '-i', 'pipe:4',
+                // Map audio & video from streams
+                '-map', '0:a',
+                '-map', '1:v',
+                // Keep encoding
+                '-c:v', 'copy',
+                // Define output file
+                // `${title}.mp4`,
+                '-f', 'matroska', 'pipe:5'
+        ], {
+                windowsHide: true,
+                stdio: [
+                        /* Standard: stdin, stdout, stderr */
+                        'inherit', 'inherit', 'inherit',
+                        /* Custom: pipe:3, pipe:4, pipe:5 */
+                        'pipe', 'pipe', 'pipe', 'pipe'
+                ],
+        });
+        ffmpegProcess.on('close', () => {
+                console.log('done');
+                // Cleanup
+                process.stdout.write('\n\n\n\n');
+                clearInterval(progressbarHandle);
+        });
 
-        // });
-        // audio.pipe(ffmpegProcess.stdio[4]);
-        // video.pipe(ffmpegProcess.stdio[5]);
+        // Link streams
+        // FFmpeg creates the transformer streams and we just have to insert / read data
+        ffmpegProcess.stdio[3].on('data', chunk => {
+                // Start the progress bar
+                if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
+                // Parse the param=value list returned by ffmpeg
+                const lines = chunk.toString().trim().split('\n');
+                const args = {};
+                for (const l of lines) {
+                        const [key, value] = l.split('=');
+                        args[key.trim()] = value.trim();
+
+                }
+                tracker.merged = args;
+        });
+        audio.pipe(ffmpegProcess.stdio[3]);
+        video.pipe(ffmpegProcess.stdio[4]);
+        ffmpegProcess.stdio[5].pipe(res);
+
 
 
 
         // ===============================================================================
-        res.header("Content-Disposition", `attachment;filename=${info}.mp4`);
-        ytdl(videoURL, {
-                filter: format => format.itag == itag
-        }).pipe(res);
+        // res.header("Content-Disposition", `attachment;filename=${info}.mp4`);
+        // ytdl(videoURL, {
+        //         filter: format => format.itag == itag
+        // }).pipe(res);
 
 })
 
